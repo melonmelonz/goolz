62ef76a1a419d458f76ea423753e55419dd66830
Cam Porterfield <fundlush@proton.me>
2026-05-14 10:21:39 -0400

    scry: deploy v1 workbench - inspect/hex/disasm/emu/trace (2026-05-14-1021)

---
+++ scry/v1/vendor/gbajs/resources/biosbin.js
@@ -0,0 +1,2 @@
+function _base64ToArrayBuffer(D){for(var r=window.atob(D),E=r.length,C=new Uint8Array(E),A=0;A<E;A++)C[A]=r.charCodeAt(A);return C.buffer}let biosBin=_base64ToArrayBuffer(`BgAA6v7//+oFAADq/v//6v7//+oAAKDhDAAA6v7//+oC86DjAABd4wHToAMg0E0CAEAt6QIAXuUEAFDjCQAACwUAUOMHAAALAEC96A7wsOEPUC3pAQOg4wDgj+IE8BDlD1C96ATwXuIQQC3pBNBN4rAQzeEBQ6DjAkyE4rAA1OGyAM3hsBDd4QEAgOGwAMThAUOg4x8AoOMA8CnhAACg4wEDxOXTAKDjAPAp4bgAVOGwEN3hABAR4AAQIRC4EEQR8///CgFDoOMCTITisgDd4bAAxOEE0I3iEIC96A==
+`);
